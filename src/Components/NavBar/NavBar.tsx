4fe90e83bfd1d35c9790c85ba1323aae8791c640
Kirubel Enyew <kirubeleneyew@gmail.com>
2022-04-14 17:21:15 +0300

    style - style and dummy data changes

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -2,24 +2,31 @@ import React from 'react'
 import './styles.css'
 import { MdOutlineShoppingBag } from 'react-icons/md'
 
-const NavBar = () => {
+interface NavProps {
+    showCart: (value: boolean) => void
+}
+
+const NavBar: React.FC<NavProps> = ({ showCart }) => {
     return (
         <nav>
             <div className='main-icons-container'>
-                <div className='main-icon'>
-                    <div className='gradient-icon' />
-                    <div className='inner-icon' />
+                <div className='icon-container'>
+                    <div className='arrow-down' />
+                    <div className='main-icon'>
+                        <div className='gradient-icon' />
+                        <div className='inner-icon' />
+                    </div>
                 </div>
                 <h4>Shopily</h4>
             </div>
             <ul>
-                <button>Home</button>
-                <button>Products</button>
-                <button>Contact</button>
-                <button>About</button>
+                <button className='navbar-button'>Home</button>
+                <button className='navbar-button'>Products</button>
+                <button className='navbar-button'>Contact</button>
+                <button className='navbar-button'>About</button>
             </ul>
             <div className='icons-container'>
-                <button className='icon-button'>
+                <button onClick={() => showCart(true)} className='icon-button'>
                     <MdOutlineShoppingBag color='black' />
                 </button>
                 <img className='profile-icon' src='https://picsum.photos/200' alt='pfp' />
--- src/Shared/products.ts
+++ src/Shared/products.ts
@@ -1,6 +1,7 @@
-type ProductKinds = 'sofa' | 'bed' | 'table' | 'tv_stand' | 'wardrobe' | 'cabinet' | 'dresser' | 'other'
+// type ProductKinds = 'sofa' | 'bed' | 'table' | 'tv_stand' | 'wardrobe' | 'cabinet' | 'dresser' | 'other'
 type ProductCategory = 'furniture' | 'electronics' | 'vehicle' | 'accessory' | 'fashion'
-interface Product {
+export interface Product {
+    id: string,
     productName: string,
     discounted: boolean,
     price: number
@@ -9,9 +10,45 @@ interface Product {
 
 export const products: Array<Product> = [
     {
+        id: '123142435464',
         discounted: true,
         price: 4699,
         productName: 'Onyx Wood Chair',
         productType: 'furniture'
-    }
+    },
+    {
+        id: '123142435465',
+        discounted: true,
+        price: 4699,
+        productName: 'Onyx Wood Sofa',
+        productType: 'furniture'
+    },
+    {
+        id: '123142435466',
+        discounted: true,
+        price: 4699,
+        productName: 'Onyx Wood Sofa',
+        productType: 'furniture'
+    },
+    {
+        id: '123142435467',
+        discounted: true,
+        price: 4699,
+        productName: 'Onyx Wood Sofa',
+        productType: 'furniture'
+    },
+    {
+        id: '123142435468',
+        discounted: false,
+        price: 4699,
+        productName: 'Onyx Wood Sofa',
+        productType: 'furniture'
+    },
+    {
+        id: '123142435469',
+        discounted: false,
+        price: 4699,
+        productName: 'Onyx Wood Sofa',
+        productType: 'furniture'
+    },
 ]
