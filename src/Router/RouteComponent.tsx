689fb39116cd3aba68b788bf0559eb6b3b3a206a
KirubelEnyew <kirastarboys@gmail.com>
2022-04-15 00:22:37 +0300

    feat & style - added new routes & cleaned up comments

--- src/Router/RouteComponent.tsx
+++ src/Router/RouteComponent.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../Components/Home/HomePage'
-// import NavBar from '../Components/NavBar/NavBar'
-// import Products from '../Components/Products/Products'
-// import { products } from '../Shared/products'
+import Checkout from '../Components/Order/Checkout'
+import OrderSummary from '../Components/Order/OrderSummary'
 
 const RouteComponent = () => {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      {/* <Route path='/' element={<Products data={products} />} /> */}
+      <Route path='/checkout' element={<Checkout />} />
+      <Route path='/summary' element={<OrderSummary />} />
     </Routes>
   )
 }
