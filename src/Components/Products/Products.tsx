772348141e32f391d5fba0264d567b42e3295b7e
Kirubel Enyew <kirubeleneyew@gmail.com>
2022-04-14 11:00:43 +0300

    feat - added product card component and styles

---
+++ src/Components/Products/Products.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Product } from '../../Shared/products'
+import './styles.css'
+import { MdCheck } from 'react-icons/md'
+interface Props {
+  data: Array<Product>
+}
+
+const Products: React.FC<Props> = ({ data }) => {
+  const [hoverFor, setHoverFor] = useState('')
+  return (
+    <div className='product-container'>
+      {data.map((product, index) => (
+        <div className='product-card' key={product.id}>
+          <img
+            src={`https://picsum.photos/200${index}`}
+            alt='product-img'
+            className='product-image'
+          />
+          {product.discounted &&
+            <div className='discount-icon-container'>
+              <div className='discount-icon'>
+                20%
+              </div>
+            </div>
+          }
+          <div className='title-price-container'>
+            <h5>{product.productName}</h5>
+            <h5>${product.price}</h5>
+          </div>
+          <button onMouseOver={() => setHoverFor(product.id)} onMouseOut={() => setHoverFor('')} className='product-button'>
+            ADD TO CART {product.id === hoverFor && <MdCheck />}
+          </button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Products
