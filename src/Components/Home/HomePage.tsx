11894a016d67ad4c40611cea93ec264a1e37d0b4
Kirubel Enyew <kirubeleneyew@gmail.com>
2022-04-15 15:59:57 +0300

    feat - added styling and filter feature

--- src/Components/Home/HomePage.tsx
+++ src/Components/Home/HomePage.tsx
@@ -2,7 +2,7 @@ import NavBar from '../NavBar/NavBar'
 import './styles.css'
 import { MdSearch, MdClose, MdRemove, MdAdd } from 'react-icons/md'
 import Products from '../Products/Products'
-import { cartData, products } from '../../Shared/products'
+import { cartData, electronicProducts, ProductKind, products } from '../../Shared/products'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -11,6 +11,196 @@ const HomePage = () => {
     const handleCartVisibility = (value: boolean) => {
         setShowCart(value)
     }
+    const [productData, setProductData] = useState(products)
+    type TabValues = 'Furniture' | 'Electronics' | 'Vehicles' | 'Accessories' | 'Fashion'
+    const [currentTab, setCurentTab] = useState<TabValues>('Furniture')
+    const [filter, setFilter] = useState<ProductKind | undefined>(undefined)
+
+    //handler functions
+
+    const tabHandler = (value: TabValues) => {
+        setCurentTab(value)
+    }
+    const filterHandler = (value: ProductKind) => {
+        if (value !== filter) {
+            setFilter(value)
+            return
+        }
+        setFilter(undefined)
+    }
+
+    // filter elements
+
+    const furnitureFilters = (
+        <ul className='filter-list'>
+            <div>
+                <input
+                    checked={filter === 'sofa'}
+                    type="checkbox"
+                    id="sofas"
+                    name="filter-group"
+                    value="sofa"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="sofas">Sofas</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'bed'}
+                    type="checkbox"
+                    id="beds"
+                    name="filter-group"
+                    value="bed"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="beds">Beds</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'table'}
+                    type="checkbox"
+                    id="tables"
+                    name="filter-group"
+                    value="table"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="tables">Tables</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'tv_stand'}
+                    type="checkbox"
+                    id="tv_stands"
+                    name="filter-group"
+                    value="tv_stand"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="tv_stands">TV Stands</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'wardrobe'}
+                    type="checkbox"
+                    id="wardrobes"
+                    name="filter-group"
+                    value="wardrobe"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="wardrobes">wardrobes</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'cabinet'}
+                    type="checkbox"
+                    id="cabinets"
+                    name="filter-group"
+                    value="cabinet"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="cabinets">Cabinets</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'dresser'}
+                    type="checkbox"
+                    id="dressers"
+                    name="filter-group"
+                    value="dresser"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="dressers">Dressers</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'other'}
+                    type="checkbox"
+                    id="other"
+                    name="filter-group"
+                    value="other"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="other">Other</label>
+            </div>
+        </ul>
+    )
+
+    const electronicsFilters = (
+        <ul className='filter-list'>
+            <div>
+                <input
+                    checked={filter === 'TV'}
+                    type="checkbox"
+                    id="TV"
+                    name="filter-group"
+                    value="TV"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="TV">TVs</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'smartPhone'}
+                    type="checkbox"
+                    id="smartPhone"
+                    name="filter-group"
+                    value="smartPhone"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="smartPhone">Smart Phones</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'laptop'}
+                    type="checkbox"
+                    id="laptop"
+                    name="filter-group"
+                    value="laptop"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="laptop">Laptops</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'headphone'}
+                    type="checkbox"
+                    id="headphone"
+                    name="filter-group"
+                    value="headphone"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="headphone">Headphones</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'speaker'}
+                    type="checkbox"
+                    id="speaker"
+                    name="filter-group"
+                    value="speaker"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="speaker">Speakers</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'mouse'}
+                    type="checkbox"
+                    id="mouse"
+                    name="filter-group"
+                    value="mouse"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="mouse">Mice</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'keyboard'}
+                    type="checkbox"
+                    id="keyboard"
+                    name="filter-group"
+                    value="keyboard"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="keyboard">Keyboards</label>
+            </div>
+            <div>
+                <input
+                    checked={filter === 'other'}
+                    type="checkbox"
+                    id="other"
+                    name="filter-group"
+                    value="other"
+                    onChange={(e: any) => filterHandler(e.target.value)} />
+                <label htmlFor="other">Other</label>
+            </div>
+        </ul>
+    )
+
     return (
         <div style={{ paddingLeft: '10px', paddingRight: '10px' }}>
             <NavBar showCart={handleCartVisibility} />
@@ -22,77 +212,102 @@ const HomePage = () => {
                     </div>
                 </div>
                 <ul className='category-list'>
-                    <button className='navbar-button'>Furniture</button>
-                    <button className='navbar-button'>Electronics</button>
-                    <button className='navbar-button'>Vehicles</button>
-                    <button className='navbar-button'>Accessories</button>
-                    <button className='navbar-button'>Fashion</button>
+                    <div>
+                        <button
+                            onClick={() => { tabHandler('Furniture'); setProductData(products) }}
+                            id={currentTab === 'Furniture' ? 'active-tab-button' : undefined}
+                            className='navbar-button'
+                        >
+                            Furniture
+                        </button>
+                        <hr style={{ width: '30%', height: '2px', borderRadius: '5px' }} color={currentTab === 'Furniture' ? 'black' : 'white'} />
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => { tabHandler('Electronics'); setProductData(electronicProducts) }}
+                            id={currentTab === 'Electronics' ? 'active-tab-button' : undefined}
+                            className='navbar-button'
+                        >
+                            Electronics
+                        </button>
+                        <hr style={{ width: '30%', height: '2px', borderRadius: '5px' }} color={currentTab === 'Electronics' ? 'black' : 'white'} />
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => { tabHandler('Vehicles'); setProductData([]) }}
+                            id={currentTab === 'Vehicles' ? 'active-tab-button' : undefined}
+                            className='navbar-button'
+                        >
+                            Vehicles
+                        </button>
+                        <hr style={{ width: '30%', height: '2px', borderRadius: '5px' }} color={currentTab === 'Vehicles' ? 'black' : 'white'} />
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => { tabHandler('Accessories'); setProductData([]) }}
+                            id={currentTab === 'Accessories' ? 'active-tab-button' : undefined}
+                            className='navbar-button'
+                        >
+                            Accessories
+                        </button>
+                        <hr style={{ width: '30%', height: '2px', borderRadius: '5px' }} color={currentTab === 'Accessories' ? 'black' : 'white'} />
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => { tabHandler('Fashion'); setProductData([]) }}
+                            id={currentTab === 'Fashion' ? 'active-tab-button' : undefined}
+                            className='navbar-button'
+                        >
+                            Fashion
+                        </button>
+                        <hr style={{ width: '30%', height: '2px', borderRadius: '5px' }} color={currentTab === 'Accessories' ? 'black' : 'white'} />
+                    </div>
                 </ul>
             </div>
             <div className='main-body'>
+
+                {/* sort and filter container */}
+
                 <div className='filter-sort-container'>
                     <span className='header'>Filter</span>
-                    <ul className='filter-list'>
-                        <div>
-                            <input type="checkbox" id="sofas" name="filter-group" value="sofas" />
-                            <label htmlFor="sofas">Sofas</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="beds" name="filter-group" value="beds" />
-                            <label htmlFor="beds">Beds</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="tables" name="filter-group" value="tables" />
-                            <label htmlFor="tables">Tables</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="tv_stands" name="filter-group" value="tv_stands" />
-                            <label htmlFor="tv_stands">TV Stands</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="wardrobes" name="filter-group" value="wardrobes" />
-                            <label htmlFor="wardrobes">wardrobes</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="cabinets" name="filter-group" value="cabinets" />
-                            <label htmlFor="cabinets">Cabinets</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="dressers" name="filter-group" value="dressers" />
-                            <label htmlFor="dressers">Dressers</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="other" name="filter-group" value="other" />
-                            <label htmlFor="other">Other</label>
-                        </div>
-                    </ul>
+                    {currentTab === 'Furniture' ?
+                        furnitureFilters
+                        :
+                        electronicsFilters
+                    }
                     <span className='header'>Sort</span>
                     <ul className='filter-list'>
                         <div>
-                            <input type="radio" id="sofas" name="sort-group" value="sofas" />
-                            <label htmlFor="sofas">Sofas</label>
+                            <input type="radio" id="discounts" name="sort-group" value="discounts" />
+                            <label htmlFor="discounts">Discounts</label>
                         </div>
                         <div>
-                            <input type="radio" id="beds" name="sort-group" value="beds" />
-                            <label htmlFor="beds">Beds</label>
+                            <input type="radio" id="bestSelling" name="sort-group" value="bestSelling" />
+                            <label htmlFor="bestSelling">Best Selling</label>
                         </div>
                         <div>
-                            <input type="radio" id="tables" name="sort-group" value="tables" />
-                            <label htmlFor="tables">Tables</label>
+                            <input type="radio" id="price-asc" name="sort-group" value="price-asc" />
+                            <label htmlFor="price-asc">Price-Low to High</label>
                         </div>
                         <div>
-                            <input type="radio" id="tv_stands" name="sort-group" value="tv_stands" />
-                            <label htmlFor="tv_stands">TV Stands</label>
+                            <input type="radio" id="price-desc" name="sort-group" value="price-desc" />
+                            <label htmlFor="price-desc">Price-Hight to Low</label>
                         </div>
                     </ul>
                 </div>
+
+                {/* product cards contianer */}
+
                 <div className='products-container'>
-                    <Products data={products} />
+                    <Products data={productData} filter={filter} />
                 </div>
             </div>
+
+            {/* pop up cart and backdrop container */}
+
             {showCart &&
-                <div className='backdrop'>
-                    <div className='cart-container'>
+                <div className='backdrop' onClick={() => setShowCart(false)}>
+                    <div id='cart-container' className='slide-in'>
                         <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
                             <button
                                 className='close-button'
